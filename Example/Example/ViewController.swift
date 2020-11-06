b9eb3b8625feaef78ca66b6f2047b769a60970f8
Luca Pizzini <luca.pizzini@digitalidea.it>
2020-11-06 09:18:46 +0100

    Updated podspec

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Luca Pizzini on 06/11/2020
+//  Copyright © 2020 Luca Pizzini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
